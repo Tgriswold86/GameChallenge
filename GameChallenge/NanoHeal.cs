2409674b86d073f7dfad4d77d01667c2efcc63cf
Tim Griswold <tgriswold86@gmail.com>
2018-10-04 11:53:55 -0400

    Random Number Generator and work on movelist

--- GameChallenge/NanoHeal.cs
+++ GameChallenge/NanoHeal.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace GameChallenge
 {
-    class NanoHeal
+    class NanoHeal : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int AmountToHeal { get; set; }
     }
 }
